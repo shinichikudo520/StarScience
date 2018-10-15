7674164c3c856ee12facfc4f560cf8e535f0ca47
shinichikudo520 <you@example.com>
2018-10-15 11:06:37 +0800

    改改改

--- transaction/config/index.js
+++ transaction/config/index.js
@@ -65,5 +65,11 @@ module.exports = {
     // `npm run build --report`
     // Set to `true` or `false` to always turn it on or off
     bundleAnalyzerReport: process.env.npm_config_report
+  },
+  devServer: {
+    proxy: {
+      '/api': 'http://67.230.191.185:8000/'
+    }
   }
+
 }
